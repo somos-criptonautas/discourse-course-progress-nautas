27de705e05b91f46dd7de6c5fab19e627e01cd9a
satoshinotdead <dev@criptonautas.co>
2026-09-09 00:01:10 -0500

    Use require_relative for the controller

--- plugin.rb
+++ plugin.rb
@@ -12,7 +12,7 @@ register_asset "stylesheets/common.scss"
 after_initialize do
   # Plugins without a Rails::Engine get no autoload path for app/, so the
   # controller has to be loaded explicitly.
-  load File.expand_path("../app/controllers/course_progress_controller.rb", __FILE__)
+  require_relative "app/controllers/course_progress_controller"
 
   Discourse::Application.routes.append do
     get "/course-progress" => "course_progress#index", :constraints => { format: "json" }
